1cd30736d16d53bf2320bf17f98cc3326dc7ee6b
anumehaagrawal <anuzenith29@gmail.com>
2018-05-11 22:24:09 +0530

    PQ added

--- CP/Assignment1-STL/Anumeha/map.cpp
+++ CP/Assignment1-STL/Anumeha/map.cpp
@@ -2,3 +2,42 @@
 using namespace std;
 
 //Your code here
+int main()
+{   
+    int t,q,y;
+    map< string,int> mp;
+    string x;
+    cin>>t;
+    while (t--)
+    {
+    	
+    	cin>>q;
+    	switch(q){
+    		case 1:
+    			cin>>x>>y;
+    			mp.insert(make_pair(x,y));
+    			break;
+    		case 2:
+    			cin>>x;
+    			cout<<mp[x]<<endl;
+    			break;
+    		case 3:
+    			cin>>x;
+    			if(mp.find(x)==mp.end()){
+    				cout<<"NO"<<endl;
+    			}
+    			else{
+    				cout<<"YES"<<endl;
+    			}
+    			break;
+    		case 4:
+    			map<string, int> :: iterator it ;
+    			for(it = mp.begin();it!=mp.end();it++){
+    				cout<<(<<it->first<<it->second<<)<< " ";
+    			}
+    			cout<<endl;
+    			break;
+    	}
+    }
+    
+}
--- CP/Assignment1-STL/Anumeha/priority_queue.cpp
+++ CP/Assignment1-STL/Anumeha/priority_queue.cpp
@@ -1,4 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int main()
+{   
+    priority_queue<pair<int, pair <int, int> > > p;
+    int t, q;
+    cin>>t;
+    while (t--)
+    {
+        cin>>q;
+        switch(q)
+        {
+            
+            case 1: 
+                    int x,y,z;
+                    cin>>x>>y>>z;
+                    p.push(make_pair(x,make_pair(y,z)));
+                    break;
+            case 2: cout<<"("<<p.top().first<<","<<"("<<p.top().second.first<<","<<p.top().second.second<<")) "<<endl;
+                    break;
+            case 3:p.pop();
+                    break;
+            case 4: cout<<p.size()<<endl;
+                    break;
 
-//Your code here
+        }
+    }
+}
